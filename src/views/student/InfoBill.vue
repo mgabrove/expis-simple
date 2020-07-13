b47aca688974dfe93d2df01a2dae8b5c9ae01c81
Marko Gabrovec <mgabrove@unipu.hr>
2020-07-13 19:50:30 +0200

    hope

---
+++ src/views/student/InfoBill.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="modal-backdrop1" style="z-index:1;">
+      <div class="modal1 col-4" role="dialog" aria-labelledby="modalTitle" aria-describedby="modalDescription">
+        <header class="modal-header" id="modalTitle">
+          <slot name="header">
+            Podaci o studentu
+          </slot>
+        </header>
+        <section class="modal-body" id="modalDescription">
+          <slot name="body">
+            Tu nešto...
+          </slot>
+        </section>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button type="button" class="btn-close" @click="approve" aria-label="Close modal">Prihvati</button>
+            <button type="button" class="btn-close" @click="decline" aria-label="Close modal">Odbij</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['user'],
+  name: 'modal',
+    data(){
+    return{
+      
+    }
+  },
+  methods: {
+    close(broj) {
+      this.$emit('close', broj)
+    },
+    approve() {
+      this.close(1)
+    },
+    decline() {
+      this.close(-1)
+    }
+  },
+};
+</script>
--- src/views/student/Phase2.vue
+++ src/views/student/Phase2.vue
@@ -4,18 +4,26 @@
             <a :href="$store.state.billKey" target="_blank" class="floating-left"><img :src="$store.getters.billUrl"></a>
             <button class="btn btn-primary shadow-none floating-left" style="background-color:#232323; border-color:#232323;">Račun</button>
         </div>
+        <InfoBill v-if="isInfoBillVisible === true" @close="closeInfoBill"/>
     </div>
 </template>
 
 <script>
+import InfoBill from '@/views/student/InfoBill'
 export default {
     name: 'phase2',
     data() {
         return {
+            isInfoBillVisible: false
         }
     },  
     methods: {
-
+        showInfoBill() {
+            this.isInfoBillVisible = true
+        },
+        closeInfoBill() {
+            this.isInfoBillVisible = false
+        },
     },
 };
 </script>
