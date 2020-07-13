65c6498214e72ecd09803a823edcc56e3b041e09
Marko Gabrovec <mgabrove@unipu.hr>
2020-07-13 20:30:51 +0200

    hope

--- src/views/app/HomePage.vue
+++ src/views/app/HomePage.vue
@@ -22,6 +22,7 @@ export default {
     },
     mounted () {
         this.$store.dispatch('retrieveInfo')
+        this.$route.meta.title = "Upisi na UniPu - "+this.$store.getters.name+" "+this.$store.getters.surname
     },
 };
 </script>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -58,8 +58,6 @@ export default {
                         console.log(err)
                     })
                 })
-            } else {
-                this.$store.state.feedback = "Trebate popuniti oba polja"
             }
         },
         enterClicked () {
